05e28b399ba6f54ff04387490bda3637be3dc065
dominic-taylor <dataylorx@gmail.com>
2017-01-31 11:30:43 +1300

    Add player panel and auto scroll down for message box

--- public/client.js
+++ public/client.js
@@ -2,13 +2,22 @@
 const $ = require('jquery');
 var socket = io();
 
+function scrollMessageBox() {
+  var box = document.getElementById('messages');
+  box.scrollTop = box.scrollHeight;
+}
+
 $('form').submit(function(){
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
+  // socket.emit('chat message', $('#m').val());
+  let m = document.getElementById('m')
+  socket.emit('chat message', m.value);
+  m.value = '';
   return false;
 });
 socket.on('chat message', function (msg) {
   $('#messages').append($('<li>').text(msg));
+  scrollMessageBox()
+
 })
 
 document.getElementById('r').addEventListener('click', function () {
@@ -17,4 +26,6 @@ document.getElementById('r').addEventListener('click', function () {
 
 socket.on('roll', function (num) {
   $('#messages').append($('<li>').text('rolled a '+num));
+  scrollMessageBox()
+
 })
