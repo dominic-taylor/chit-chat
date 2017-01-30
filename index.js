d2228fd037af23f092f8cc0d1ed7a054df79389c
dominic-taylor <dataylorx@gmail.com>
2017-01-30 16:44:00 +1300

    Refactored to seperate concerns

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const app = require('express')();
+var express = require('express');
+var path = require('path');
+var app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+// Define the port to run on
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname+'/index.html')
+  res.status(200).send('ok')
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 })
+
 
 io.on('connection', function (socket) {
   console.log('a user connected');
