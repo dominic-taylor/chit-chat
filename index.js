21cc6b66c8f145550cf3a142ae4d1cb9d41d39c7
dominic-taylor <dataylorx@gmail.com>
2017-02-01 19:03:24 +1300

    Add user can choose nickname

--- index.js
+++ index.js
@@ -14,19 +14,31 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
+const users = []
 
 io.on('connection', function (socket) {
   console.log('a user connected');
+  socket.on('set user name', function (data) {
+    if(users.indexOf(data.userName) > -1){
+      io.emit('publish', {userName: 'NOTICE::', message:' Sorry, '+data.userName+' is taken'})
+    }
+    else{
+      users.push(data.userName)
+      io.emit('added user', {users:users, newPlayer:data.userName})
+    }
+  })
+
   socket.on('disconnect', function () {
     console.log('user disconnected');
   })
-  socket.on('message', function(player) {
-    console.log(player.userName +' '+player.message);
-    io.emit('publish', player)
+  socket.on('message', function(data) {
+    console.log(data.userName +' '+data.message);
+    io.emit('publish', data)
   })
-  socket.on('roll', function(player) {
+  socket.on('roll', function(data) {
+    console.log(data.userName+' rolled');
     var num = Math.floor(Math.random() * (12 - 2 + 1)) + 2;
-    io.emit('rolled', player, num)
+    io.emit('rolled', data, num)
   })
 
 })
--- public/client.js
+++ public/client.js
@@ -13,21 +13,38 @@ function addMessage(message) {
 
 document.getElementById("chatbar").addEventListener("submit", function(e){
   let text = document.getElementById('m')
-  var name = 'Placeholder chatter'
+  let name = document.getElementById('this-player').innerHTML
   socket.emit('message', {userName:name, message:text.value});
   text.value = '';
   e.preventDefault();
 });
 
-socket.on('publish', function (player) {
-  addMessage(player.userName+ ":  " +player.message)
+document.getElementById("name").addEventListener("submit", function(e){
+  let name = document.getElementById('n').value
+  socket.emit('set user name', {userName:name});
+  e.preventDefault();
+  //Once user has chosen name, allow to change name? 
+});
+
+socket.on('added user', function(data){
+  //update plauer list
+  document.getElementById('this-player').innerHTML = data.newPlayer
+  document.getElementById('player-list').innerHTML = data.users
+
+  //notify all users a new player has joined.
+  socket.emit('message', {userName:'NOTICE:', message:data.newPlayer+' has joined'})
+
+})
+
+socket.on('publish', function (data) {
+  addMessage(data.userName+ ":  " +data.message)
 })
 
 document.getElementById('r').addEventListener('click', function () {
-  var p = 'Placeholder player'
-  socket.emit('roll', {userName:p})
+  var name = document.getElementById('this-player').innerHTML
+  socket.emit('roll', {userName:name})
 }, false)
 
-socket.on('rolled', function (player, num) {
-  addMessage(player.userName + ' rolled a '+num)
+socket.on('rolled', function (data, num) {
+  addMessage(data.userName + ' rolled a '+num)
 })
