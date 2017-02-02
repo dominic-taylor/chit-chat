80bbfd7136d1b6e151b361e2b7bb7221f121228c
dominic-taylor <dataylorx@gmail.com>
2017-02-02 17:39:18 +1300

    Add multi user chat

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+'use strict'
+const express = require('express');
+const path = require('path');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+const port = process.env.PORT || 3000;
 // Define the port to run on
-app.set('port', 3000);
+app.set('port', port);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,36 +22,87 @@ const users = []
 // #890890{userName: PlayerJO}
 //
 
+let gameCollection = new function () {
+  this.totalGameCount = 0,
+  this.gameList = {}
+}
+
+let numUsers = 0;
 
 io.on('connection', function (socket) {
+  let addedUser = false;
+
   console.log('a user connected');
-  socket.on('set user name', function (data) {
-    console.log(socket.userName);
-    if(users.indexOf(data.userName) > -1){
-      io.emit('publish', {userName: 'NOTICE::', message:' Sorry, '+data.userName+' is taken'})
-    }
-    else{
-      users.push(data.userName)
-      io.emit('added user', {users:users, newPlayer:data.userName})
-    }
+
+  socket.on('message', function(data) {
+
+    io.emit('publish', {
+      username: socket.username,
+      message: data
+    })
+  })
+
+  socket.on('set user name', function (username) {
+    if(addedUser) return;
+    socket.username = username
+    numUsers++
+    addedUser = true;
+    socket.emit('login', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    })
   })
 
   socket.on('disconnect', function () {
-    console.log('user disconnected');
+    if(addedUser){
+      --numUsers;
+
+      console.log('user disconnected');
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      })
+    }
   })
-  socket.on('message', function(data) {
-    console.log(data.userName +' '+data.message);
-    io.emit('publish', data)
+
+
+  socket.on('makeGame', function () {
+      let gameId = (Math.random()+1).toString(36).slice(2, 18);
+      console.log('Game #'+gameId+' created by '+socket.username);
+
+      gameCollection.gameList.gameId = gameId
+      gameCollection.gameList.gameId.playerOne = socket.username
+      gameCollection.gameList.gameId.open = true
+      gameCollection.totalGameCount++;
+
+      io.emit('gameCreated', {
+        username: socket.username,
+        gameId: gameId
+      })
+
+
   })
+
   socket.on('roll', function(data) {
-    console.log(data.userName+' rolled');
+    console.log(data+' rolled');
     var num = Math.floor(Math.random() * (12 - 2 + 1)) + 2;
     io.emit('rolled', data, num)
   })
 
 })
 
-
-http.listen(3000, function () {
-  console.log('listening on port:3000');
+function joinGame(username, game) {
+  if(game.player2 !== null){
+    game.player2 = username
+  }else{
+    console.log('game #'+game.id+" alread y has max players");
+  }
+}
+http.listen(port, function () {
+  console.log('listening on port:%d', port);
 })
--- public/client.js
+++ public/client.js
@@ -14,14 +14,15 @@ function addMessage(message) {
 document.getElementById("chatbar").addEventListener("submit", function(e){
   let text = document.getElementById('m')
   let name = document.getElementById('this-player').innerHTML
-  socket.emit('message', {userName:name, message:text.value});
+  socket.emit('message', text.value); //needs to be sanitized for security
   text.value = '';
   e.preventDefault();
 });
 
-document.getElementById("name").addEventListener("submit", function(e){
+document.getElementById("join").addEventListener("click", function(e){
   let name = document.getElementById('n').value
-  socket.emit('set user name', {userName:name});
+  if(name.length<1){name = 'Player#'+Math.floor(Math.random() * (100 - 2 + 1)) + 2}
+  socket.emit('set user name', name);
   e.preventDefault();
   //Once user has chosen name, allow to change name?
 });
@@ -32,21 +33,52 @@ socket.on('added user', function(data){
   for (var i = 0; i < data.users.length; i++) {
     document.getElementById('player-list').innerHTML += '<li>'+data.users[i]
   }
-
   //notify all users a new player has joined.
   socket.emit('message', {userName:'NOTICE:', message:data.newPlayer+' has joined'})
-
 })
 
 socket.on('publish', function (data) {
-  addMessage(data.userName+ ":  " +data.message)
+  console.log(JSON.stringify(data));
+  addMessage(data.username+ ":  " +data.message)
 })
 
 document.getElementById('r').addEventListener('click', function () {
   var name = document.getElementById('this-player').innerHTML
-  socket.emit('roll', {userName:name})
+  socket.emit('roll', name)
 }, false)
 
 socket.on('rolled', function (data, num) {
-  addMessage(data.userName + ' rolled a '+num)
+  addMessage(data + ' rolled a '+num)
 })
+
+socket.on('login', function (data) {
+  connected = true;
+  let message = 'Welcome to the Game!';
+  document.getElementById('this-player').innerHTML = data.username
+  addMessage(message);
+  addParticipantsMessage(data);
+
+})
+
+socket.on('user joined', function (data) {
+  addMessage(data.username+ ' joined!')
+  addParticipantsMessage(data)
+})
+
+socket.on('user left', function (data) {
+  addMessage(data.username+ ' left the lobby.')
+  addParticipantsMessage(data)
+})
+
+function addParticipantsMessage(data) {
+  let message = ''
+  if(data.numUsers === 1){
+    message+=' One person in game'
+  }else{
+    message+= ' There are '+ data.numUsers+ ' players in the lobby'
+  }
+  addMessage(message)
+  // for (var i = 0; i < data.users.length; i++) {
+  //   document.getElementById('player-list').innerHTML += '<li>'+data.users[i]
+  // }
+}
